3a2c230ccd5abcbb4293a85e967d3c5a7dfb4c3d
peeb01 <kitisaiyot01@gmail.com>
2025-01-13 02:48:27 +0700

    email verfication update to send verify code into email.

--- controller/register.go
+++ controller/register.go
@@ -4,15 +4,15 @@ import (
 	"book/model"
 	"crypto/rand"
 	"encoding/hex"
-	// "fmt"
+	"fmt"
 	"net/http"
-	// "github.com/joho/godotenv"
-	// "os"
-	// "log"
+	"github.com/joho/godotenv"
+	"os"
+	"log"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
-	// "gopkg.in/gomail.v2"
+	"gopkg.in/gomail.v2"
 )
 
 func Register(db *gorm.DB, c *fiber.Ctx) error {
@@ -42,11 +42,11 @@ func Register(db *gorm.DB, c *fiber.Ctx) error {
 		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": "cannot register new user"})
 	}
 
-	// if err := sendVerificationEmail(req.Email, token); err != nil {
-	// 	return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": "failed to send verification email"})
-	// }
+	if err := sendVerificationEmail(req.Email, token); err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": "failed to send verification email"})
+	}
 
-	return c.Status(http.StatusCreated).JSON(fiber.Map{"message": token})
+	return c.Status(http.StatusCreated).JSON(fiber.Map{"message":"Check Verify in your Email."})
 }
 
 // create token
@@ -56,34 +56,35 @@ func generateVerificationToken() string {
 	return hex.EncodeToString(b)
 }
 
-// // sendVerificationEmail ส่งอีเมลยืนยัน
-// func sendVerificationEmail(email, token string) error {
-//     // โหลดตัวแปรจาก .env
-//     if err := godotenv.Load(); err != nil {
-//         log.Fatal("Error loading .env file")
-//     }
+// send authen email
+func sendVerificationEmail(email, token string) error {
+	// .env
+    if err := godotenv.Load(); err != nil {
+        log.Fatal("Error loading .env file")
+    }
 
-//     gmail := os.Getenv("GMAIL")
-//     password := os.Getenv("GPASS")
+    gmail := os.Getenv("GMAIL")
+    password := os.Getenv("GPASS")
+	host := os.Getenv("DOMAIN")
 
-//     if gmail == "" || password == "" {
-//         log.Fatal("Environment variables GMAIL or GPASS are missing")
-//     }
+    if gmail == "" || password == "" {
+        log.Fatal("Environment variables GMAIL or GPASS are missing")
+    }
 
-//     m := gomail.NewMessage()
-//     m.SetHeader("From", gmail)
-//     m.SetHeader("To", email)
-//     m.SetHeader("Subject", "Email Verification")
-//     m.SetBody("text/plain", fmt.Sprintf("Your verification token is: %s", token))
+    m := gomail.NewMessage()
+    m.SetHeader("From", gmail)
+    m.SetHeader("To", email)
+    m.SetHeader("Subject", "Email Verification")
+    m.SetBody("text/plain", fmt.Sprintf("Your verification token: %s/verify?token=%s", host, token))
 
-//     // ตั้งค่า SMTP สำหรับ Gmail
-//     d := gomail.NewDialer("smtp.gmail.com", 587, gmail, password)
+    // ตั้งค่า SMTP สำหรับ Gmail
+    d := gomail.NewDialer("smtp.gmail.com", 587, gmail, password)
 
-//     if err := d.DialAndSend(m); err != nil {
-//         log.Println("Failed to send email:", err)
-//         return err
-//     }
+    if err := d.DialAndSend(m); err != nil {
+        log.Println("Failed to send email:", err)
+        return err
+    }
 
-//     log.Println("Verification email sent successfully to:", email)
-//     return nil
-// }
+    log.Println("Verification email sent successfully to:", email)
+    return nil
+}
